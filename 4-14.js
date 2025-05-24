fbfed926e4b00ba7ff9f0ab4d41daeb607c9cddc
poxc-1999 <changhyun75@naver.com>
2025-05-24 17:31:05 +0900

    Promise와 then 체이닝을 이용하여 비동기 작업을 순차적으로 처리하는 예제 (커피 주문)

--- 4-14.js
+++ 4-14.js
@@ -1,15 +1,34 @@
-var addCoffee = function(name) {
-    return function(prevName) {
+new Promise(function(resolve) {
+    setTimeout(function() {
+      var name = '에스프레소';
+      console.log(name);
+      resolve(name);
+    }, 500);
+  })
+    .then(function(prevName) {
       return new Promise(function(resolve) {
         setTimeout(function() {
-          var newName = prevName ? prevName + ', ' + name : name;
-          console.log(newName);
-          resolve(newName);
+          var name = prevName + ', 아메리카노';
+          console.log(name);
+          resolve(name);
         }, 500);
       });
-    };
-  };
-  addCoffee('에스프레소')()
-    .then(addCoffee('아메리카노'))
-    .then(addCoffee('카페모카'))
-    .then(addCoffee('카페라떼'));
+    })
+    .then(function(prevName) {
+      return new Promise(function(resolve) {
+        setTimeout(function() {
+          var name = prevName + ', 카페모카';
+          console.log(name);
+          resolve(name);
+        }, 500);
+      });
+    })
+    .then(function(prevName) {
+      return new Promise(function(resolve) {
+        setTimeout(function() {
+          var name = prevName + ', 카페라떼';
+          console.log(name);
+          resolve(name);
+        }, 500);
+      });
+    });
